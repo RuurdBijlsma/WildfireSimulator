29d7ff7e401d1fcc3c00051233b5966c995e6331
Ruurd Bijlsma <ruurd@bijlsma.dev>
2021-10-26 18:59:51 +0200

    Load and cache fire list metadata for random sample selection

--- new/load_all.py
+++ new/load_all.py
@@ -1,4 +1,4 @@
-from load_fire import load_fire_gdf, gdf_to_bounds, pick_fire_id
+from load_fire import load_fire_gdf, gdf_to_bounds, bounds_to_square, pick_fire_id
 from load_weather import load_weather
 from load_height import load_height
 from bounds_restrictions import get_restrictions
@@ -9,7 +9,7 @@ bounds_restrictions = get_restrictions()
 fire_id = pick_fire_id(bounds_restrictions)
 # get bounds of fire
 fire_gdf = load_fire_gdf(fire_id)
-bounds = gdf_to_bounds(fire_gdf)
+bounds = bounds_to_square(gdf_to_bounds(fire_gdf))
 # get data
 weather_data = load_weather(bounds)
 height_data = load_height(bounds)
--- new/load_fire.py
+++ new/load_fire.py
@@ -1,6 +1,9 @@
 import os
-from data_paths import glob_fire_path
+import random
+
+from data_paths import glob_fire_path, fire_meta_path
 import geopandas
+import numpy as np
 
 
 # Polygon zit klinkklaar in die geometry
@@ -15,11 +18,57 @@ import geopandas
 # automatisch training?
 
 def pick_fire_id(bounds):
-    id = geopandas.read_file(glob_fire_path, rows=slice(0, 1)).iloc[0]["Id"]
+    if not os.path.isfile(fire_meta_path):
+        generate_fire_meta()
+    fire_list = np.load(fire_meta_path)
+    random_index = random.randrange(fire_list.shape[0])
+    print('loaded fire meta')
     # loop through fires
     # per fire get bounds of fire cropped gdf
     # check if bounds are full within restricted bounds parameter
-    return id
+    pass
+
+
+def generate_fire_meta():
+    index = 0
+    batch_size = 10000
+    current_id = -1
+    id_start_offset = 0
+
+    # all_ids: fire_id, row_start_index, row_end_index, bounds(left,right,bottom,top),
+    all_ids = np.empty((0, 7), dtype=np.float)
+    while True:
+        gdf = geopandas.read_file(
+            glob_fire_path,
+            rows=slice(index, index + batch_size)
+        )
+        if gdf.empty:
+            break
+
+        sub_offset = 0
+        for fire_id in gdf.values[:, 2]:
+            sub_offset += 1
+            if current_id == fire_id:
+                continue
+            if current_id != -1:
+                id_end_offset = index + sub_offset
+                bounds = gdf_to_bounds(geopandas.read_file(
+                    glob_fire_path, rows=slice(id_start_offset, id_end_offset)
+                ))
+                new_id_row = np.array([
+                    current_id, id_start_offset, id_end_offset,
+                    bounds['left'], bounds['right'], bounds['bottom'], bounds['top']
+                ])
+                all_ids = np.vstack([all_ids, new_id_row])
+            current_id = fire_id
+            id_start_offset = index + sub_offset
+
+        index += batch_size
+        print(f"Scanning gdf... {index} rows scanned, {all_ids.shape[0]} fires found")
+
+    print(f"Saving fire metadata to file {fire_meta_path}")
+    np.save(fire_meta_path, all_ids)
+    print(all_ids)
 
 
 def load_fire_gdf(fire_id):
@@ -45,18 +94,38 @@ def load_fire_gdf(fire_id):
 
 
 def gdf_to_bounds(gdf):
+    if gdf.shape[0] == 1:
+        return {
+            "left": gdf.bounds['minx'].iloc[0],
+            "right": gdf.bounds['maxx'].iloc[0],
+            "bottom": gdf.bounds['miny'].iloc[0],
+            "top": gdf.bounds['maxy'].iloc[0],
+        }
     minx = min(*gdf.bounds['minx'])
     miny = min(*gdf.bounds['miny'])
     maxx = max(*gdf.bounds['maxx'])
     maxy = max(*gdf.bounds['maxy'])
-    width = maxx - minx
-    height = maxy - miny
-    center_y = miny + (maxy - miny) / 2
-    center_x = minx + (maxx - minx) / 2
+    return {
+        "left": minx,
+        "right": maxx,
+        "bottom": miny,
+        "top": maxy,
+    }
+
+
+def bounds_to_square(bounds):
+    left = bounds['left']
+    right = bounds['right']
+    top = bounds['top']
+    bottom = bounds['bottom']
+    width = right - left
+    height = top - bottom
+    center_y = bottom + (top - bottom) / 2
+    center_x = left + (right - left) / 2
     if width > height:
         grid_bounds = {
-            "left": minx,
-            "right": maxx,
+            "left": left,
+            "right": right,
             "bottom": center_y - width / 2,
             "top": center_y + width / 2,
         }
@@ -64,8 +133,8 @@ def gdf_to_bounds(gdf):
         grid_bounds = {
             "left": center_x - width / 2,
             "right": center_x + width / 2,
-            "bottom": miny,
-            "top": maxy,
+            "bottom": bottom,
+            "top": top,
         }
     print("bounds is now a square around the area affected by the fire in `gdf`", grid_bounds)
     return grid_bounds
