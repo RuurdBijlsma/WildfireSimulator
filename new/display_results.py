94542073c9ef61c9b7f151b497f7bc15e96777d7
Ruurd Bijlsma <ruurd@bijlsma.dev>
2022-02-15 17:08:29 +0100

    Add plot params to display_results.py

--- new/display_results.py
+++ new/display_results.py
@@ -3,6 +3,57 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def plot_params():
+    params_per_fold = [
+        np.load("results/fold1_params.npy"),
+        np.load("results/fold2_params.npy"),
+        np.load("results/fold3_params.npy"),
+        np.load("results/fold4_params.npy"),
+        np.load("results/fold5_params.npy"),
+    ]
+    params_labels = ["burnRate", "heightEffectMultiplierUp", "heightEffectMultiplierDown", "windEffectMultiplier",
+                     "activityThreshold", "spreadSpeed", "deathRate", "areaEffectMultiplier", "fireDeathThreshold",
+                     "cellArea"]
+    plot_param_values(params_per_fold, params_labels, "param")
+
+    lcr_per_fold = [
+        np.load("results/fold1_land_cover_rates.npy"),
+        np.load("results/fold2_land_cover_rates.npy"),
+        np.load("results/fold3_land_cover_rates.npy"),
+        np.load("results/fold4_land_cover_rates.npy"),
+        np.load("results/fold5_land_cover_rates.npy"),
+    ]
+    lcr_labels = ["Continuous urban fabric", "Discontinuous urban fabric", "Industrial or commercial units",
+                  "Road and rail networks", "Port areas", "Airports", "Mineral extraction sites",
+                  "Dump sites", "Construction sites", "Green urban areas", "Sport and leisure facilities",
+                  "Non-irrigated arable land", "Permanently irrigated land", "Rice fields", "Vineyards",
+                  "Fruit trees and berry plantations", "Olive groves", "Pastures",
+                  "Annual crops", "Complex cultivation patterns",
+                  "Agriculture",
+                  "Agro-forestry areas", "Broad-leaved forest", "Coniferous forest", "Mixed forest",
+                  "Natural grasslands", "Moors and heathland", "Sclerophyllous vegetation",
+                  "Transitional woodland-shrub", "Beaches, dunes, sands", "Bare rocks", "Sparsely vegetated areas",
+                  "Burnt areas", "Glaciers and perpetual snow", "Inland marshes", "Peat bogs", "Salt marshes",
+                  "Salines", "Intertidal flats", "Water courses", "Water bodies", "Coastal lagoons", "Estuaries",
+                  "Sea and ocean", "NODATA", "UNCLASSIFIED LAND SURFACE", "UNCLASSIFIED WATER BODIES"]
+    plot_param_values(lcr_per_fold, lcr_labels, "lcr", True)
+
+
+def plot_param_values(values, labels, name, wide=False):
+    for i, data in enumerate(values):
+        plt.scatter(labels, data, label=f"Fold {i + 1}")
+    fig = plt.gcf()
+    fig.set_size_inches(15 if wide else 5, 7 if wide else 5)
+    fig.set_dpi(200)
+    plt.xlabel("Parameter")
+    plt.ylabel("Optimized value")
+    plt.legend()
+    plt.xticks(rotation=60, ha='right')
+    plt.tight_layout(h_pad=1)
+    plt.savefig(f"param_plot_{name}.png", dpi=300)
+    plt.show()
+
+
 def plot_cuda():
     times = [5942, 4828, 4311, 3912, 3732, 4321, 3932, 3981, 3946, 3489, 3992, 3591, 3210, 3456, 3473, 3475, 3429, 3544,
              3993, 3850, 3500, 4090, 3905, 3866, 3827, 3999, 4291, 4123, 4222, 4536, 4378, 4314, 4284, 4526, 4838, 4754,
@@ -66,4 +117,4 @@ def plot_gpu():
 
 # plot_results()
 # plot_cuda()
-plot_gpu_vs_cpu()
+plot_params()
